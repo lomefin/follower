05fb1cff55288a97f79dbd7d0f46044143abd7ce
Jose Tomas Robles Hahn <jtrh@jtrh.example.com>
2013-09-26 16:26:58 -0300

    Follower 1.1 now complies with EPIC Follower Input Handler v1.1 ('follower:v1.1')

--- follower.js
+++ follower.js
@@ -6,10 +6,13 @@ FollowerLogger = (function() {
 
   FollowerLogger.log = function(message) {
     var entry;
-    var currentdate = new Date(); 
+    var currentdate = new Date();
+    var time = currentdate.toLocaleTimeString();
+    /*
     var time =  currentdate.getHours() + ":"  
                     + currentdate.getMinutes() + ":" 
                     + currentdate.getSeconds(); + " > "
+    */
     var moment = $('<span>').text(time);
     var msg = $('<span>').text(message);
     entry = $('<p class="console-entry">').append(moment).append(msg);
@@ -69,48 +72,58 @@ Follower = (function() {
         window.localStorage.setItem("deviceName",data);
         FollowerLogger.log(data);
       }); 
-    }else
+    } else
     {
       this.log('Machine is ' + window.localStorage.getItem('deviceName'))
-    }
+    };
     if(!!window.localStorage.getItem("positions"))
     {
       window.positions = $.parseJSON(window.localStorage.getItem("positions"));
-    }else
+    } else
     {
       window.positions = [];
-    }
+    };
+
     setInterval(function(){
       window.localStorage.setItem("positions",JSON.stringify(window.positions));  
-    },60000);
+    }, 60000);
 
     setInterval(function(){
       var p = window.positions;
       var ok = true;
       if(!!p && p.length > 0 && ok)
       {
+        var position_to_send = p.shift();
+        position_to_send.time_sent = new Date().toISOString()
         var options = {
-          url : 'http://www.routing.uc.cl/log_gps',
+          url : 'http://api.routing.uc.cl/trackpoints/?bind',
           type:'POST',
-          data: p[0]
-        }
-        $.ajax(options).done(function(data){
-          
-          for(i = 0; i < p.length; i++)
-          {
-            if (!!p[i] && p[i].timestamp == data.timestamp)
+          /* data: JSON.stringify(p[0]), */
+          data: JSON.stringify(position_to_send),
+          contentType: 'application/json',
+          context: position_to_send
+        };
+        $.ajax(options).done(
+          function() {
+            console.log("(bind) Successfully sent:", this.position, 'Queue length:', p.length);
+            /* for(i = 0; i < p.length; i++)
             {
-              p.splice(i,1);
+              if (!!p[i] && p[i].timestamp == data.timestamp)
+              {
+                p.splice(i,1);
 
-            }
+              };
+            }; */
           }
-        }).error(function(){
-          console.warn("Failed sending");
-          
-        });   
+        ).fail(
+          function() {
+            p.unshift(this);
+            console.warn("(bind) Failed sending. Queue length: ", p.length);
+        });
       }
       
-    },6000);
+    }, 6000);
+
     return true;
   };
 
@@ -127,7 +140,11 @@ Follower = (function() {
       window.localStorage.setItem('segment',parseInt(window.localStorage.getItem('segment'))+1);
     }
 
-    this.watchId = navigator.geolocation.watchPosition(function(){console.log("Success")},function(){console.log("Error",arguments)},{frequency: 19000, enableHighAccuracy:true});
+    this.watchId = navigator.geolocation.watchPosition(
+      function() {console.log("Success (watchPosition)")},
+      function() {console.log("Error (watchPosition)",arguments)},
+      {frequency: 19000, enableHighAccuracy:true}
+    );
     _this.currentPosition();
     
     this.intervalId = setInterval(_this.currentPosition,10000);
@@ -144,47 +161,80 @@ Follower = (function() {
   };
 
   Follower.prototype.currentPosition = function() {
-    console.log("currentPosition");
+    //console.log("currentPosition");
     //if (window.localStorage.getItem("status") != "tracking" ) return ;
     _this = this;
     
     navigator.geolocation.getCurrentPosition(function(position){
       window.lastPosition = position;
       var positionString = position.coords.latitude + " , " + position.coords.longitude 
-      var timestamp = new Date(position.timestamp);
-      var accuracy = position.coords.accuracy;
+      // var timestamp = new Date(position.timestamp);
+      // var accuracy = position.coords.accuracy;
       var dataToSend = {
+              version: 'follower:v1.1',
               sender:window.localStorage.getItem('deviceName'),
               segment:window.localStorage.getItem('segment'),
-              position: positionString ,
-              accuracy: accuracy,
-              timestamp: timestamp
+              time_sent: 'TIME_SENT__NOT_SET_YET',
+              position: {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                time_captured: new Date(position.timestamp).toISOString(),
+                accuracy: position.coords.accuracy,
+                speed: position.coords.speed
+              }
           }
-      console.log(dataToSend);
+      // console.log(dataToSend);
       if(!window.positions){ 
         window.positions = [];
-      }    
-      window.positions.push(dataToSend);
-      
-      var options = {
-          url : 'http://www.routing.uc.cl/log_gps',
-          type:'POST',
-          data: dataToSend
       }
-      FollowerLogger.log(positionString);
-      $.ajax(options).done(function(data){
-        console.log("Done. Data: ", data.timestamp);
-        var p = window.positions;
-        for(i = 0; i < p.length; i++)
-        {
-          if (!!p[i] && p[i].timestamp == data.timestamp)
-          {
-            console.log("Removing ",i,p[i]);
-            p.splice(i,1);
+      /*
+      window.positions.push(dataToSend);
 
-          }
+      var options = {
+          // url : 'http://www.routing.uc.cl/log_gps',
+          url: 'http://api.routing.uc.cl/trackpoints/?currentpos',
+          type:'POST',
+          data: JSON.stringify(dataToSend)
+      }
+      */
+      position_to_send = dataToSend;
+
+      position_to_send.time_sent = new Date().toISOString()
+
+      var options = {
+        url : 'http://api.routing.uc.cl/trackpoints/?currentpos',
+        type:'POST',
+        /* data: JSON.stringify(p[0]), */
+        data: JSON.stringify(position_to_send),
+        contentType: 'application/json',
+        context: position_to_send
+      };
+
+      FollowerLogger.log(positionString);
+      var p = window.positions;
+
+      $.ajax(options).done(
+        function(data) {
+          // console.log("Done (currentpos). Data: ", data.timestamp);
+          console.log("(currentPos) Successfully sent:", this.position, 'Queue length:', p.length);
+          /* var p = window.positions;
+          for(i = 0; i < p.length; i++)
+          {
+            if (!!p[i] && p[i].timestamp == data.timestamp)
+            {
+              console.log("Removing ",i,p[i]);
+              p.splice(i,1);
+
+            };
+          };
+          */
         }
-      }); 
+      ).fail(
+        function() {
+          p.push(this);
+          console.warn("(currentPos) Failed sending. Queue length:", p.length);
+        }
+      );
 
     });
 
